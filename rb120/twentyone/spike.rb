bd97a9b929dc066ec5aa25345d67764ff0c64a33
mguilli <matthew.guilliams@gmail.com>
2021-10-19 06:48:35 -0500

    Finished 21 spike

--- rb120/twentyone/spike.rb
+++ rb120/twentyone/spike.rb
@@ -1,28 +1,10 @@
 # frozen_string_literal: true
 
-# Twenty-One is a card game consisting of a dealer and a player, where the participants try to get as close to 21 as possible without going over.
-# Here is an overview of the game:
-# - Both participants are initially dealt 2 cards from a 52-card deck.
-# - The player takes the first turn, and can "hit" or "stay".
-# - If the player busts, he loses. If he stays, it's the dealer's turn.
-# - The dealer must hit until his cards add up to at least 17.
-# - If he busts, the player wins. If both player and dealer stays, then the highest total wins.
-# - If both totals are equal, then it's a tie, and nobody wins.
-
-NUMBERS = (2..10).to_a
-FACES = %w(J Q K A)
-ACE = 'A'
-SUITS = %w(C D H S)
-FACE_VALUE = 10
-ACE_VALUE = 1
-STARTING_HAND_SIZE = 2
-BUST = 21
-
 # Card object
 class Card
-  WRAP_LEFT = '['
-  WRAP_RIGHT = ']'
-  SEPERATOR = '-'
+  ACE = 'A'
+  FACE_VALUE = 10
+  ACE_VALUE = 1
 
   attr_reader :face, :suit, :value
 
@@ -37,17 +19,17 @@ class Card
   end
 
   def to_s
-    "[#{face}-#{suit}]"
+    "[#{face}#{suit}]"
   end
 
   def self.hidden
-    "[?-?]"
+    "[??]"
   end
 
   private
 
   def card_value
-    return(face) unless FACES.include? face
+    return(face) unless Deck::FACES.include? face
 
     face == ACE ? ACE_VALUE : FACE_VALUE
   end
@@ -55,9 +37,12 @@ end
 
 # Tracks the remaining cards in a deck
 class Deck
+  NUMBERS = (2..10).to_a
+  FACES = %w(J Q K A)
+  SUITS = %w(♥ ♠ ♦ ♣)
+
   def initialize
-    cards = (NUMBERS + FACES).product(SUITS)
-    @cards = cards.map { |attributes| Card.new(*attributes) }
+    @cards = []
     shuffle
   end
 
@@ -69,16 +54,26 @@ class Deck
   private
 
   def shuffle
-    cards.shuffle!
+    card_array = (NUMBERS + FACES).product(SUITS)
+    self.cards += card_array.map { |attributes| Card.new(*attributes) }.shuffle
   end
 
-  attr_reader :cards
+  attr_accessor :cards
 end
 
 # Tracks player hands
 class Participant
+  BUST = 21
+
+  include Comparable
+
+  attr_reader :name
+
   def initialize
     @name = choose_name
+  end
+
+  def discard_hand
     @hand = []
   end
 
@@ -87,22 +82,27 @@ class Participant
   end
 
   def show_hand(hide_cards: false)
-    first_card = hide_cards ? Card.new('?', '?'): @hand.first
-
+    first_card = hide_cards ? Card.new('?', '?') : @hand.first
     cards = @hand[1..-1].map(&:to_s).join
-    print "#{@name}: #{first_card}#{cards}"
-    puts(hide_cards ? '' : " = #{hand_total}")
+    total = hide_cards ? '' : " = #{hand_total}"
+    status = busted? ? ' *busted*' : ''
+
+    puts "#{@name}: #{first_card}#{cards}#{total}#{status}"
   end
 
   def busted?
     hand_total > BUST
   end
 
-  private
+  def <=>(other)
+    hand_total <=> other.hand_total
+  end
+
+  protected
 
   def hand_total
     total = @hand.sum(&:value)
-    total_max_ace = total + FACE_VALUE
+    total_max_ace = total + Card::FACE_VALUE
     total = total_max_ace if @hand.any?(&:ace?) && (total_max_ace <= BUST)
 
     total
@@ -110,47 +110,19 @@ class Participant
 end
 
 class Dealer < Participant
+  STAY_ON = 17
+
   def choose_name; 'Dealer'; end
+
+  def stay?
+    hand_total >= STAY_ON || busted?
+  end
 end
 
 class Player < Participant
   def choose_name; 'Player'; end
-end
 
-# Contains the game logic particular to the Twenty-One game
-class Game
-  def initialize
-    @deck = Deck.new
-    @player = Player.new
-    @dealer = Dealer.new
-  end
-
-  def play
-    deal_starting_hand
-    show_hands
-    player_turn
-    # dealer_turn unless player.busted?
-    # show_hands(hide_cards: false)
-    # determine_winner
-  end
-
-  private
-
-  attr_reader :player, :dealer, :deck
-
-  def player_turn
-    loop do
-      choice = prompt_player
-      break if choice == 's'
-
-      player.draw_card(deck)
-      break if player.busted?
-
-      show_hands
-    end
-  end
-
-  def prompt_player
+  def prompt
     choice = nil
     loop do
       puts "Hit or Stay (h/s)?"
@@ -162,16 +134,92 @@ class Game
     choice
   end
 
+  def continue?
+    choice = nil
+    loop do
+      print 'Play again (y/n)?'
+      choice = gets.chomp
+      break if %w(y n).include? choice
+
+      puts 'Invalid selection...'
+    end
+    choice == 'y'
+  end
+end
+
+# Contains the game logic particular to the Twenty-One game
+class Game
+  STARTING_HAND_SIZE = 2
+  MENU_WIDTH = 30
+
+  def initialize
+    @deck = Deck.new
+    @player = Player.new
+    @dealer = Dealer.new
+  end
+
+  def play
+    loop do
+      discard_hands
+      play_round
+      puts '-' * MENU_WIDTH
+      break unless player.continue?
+    end
+    puts 'Goodbye!'
+  end
+
+  def discard_hands
+    [player, dealer].each(&:discard_hand)
+  end
+
+  private
+
+  attr_reader :player, :dealer, :deck
+
+  def play_round
+    deal_starting_hand && show_hands
+    player_turn
+    dealer_turn unless player.busted?
+    show_hands(hide_cards: false)
+    puts winner
+  end
+
+  def winner
+    if [player, dealer].any?(&:busted?)
+      "#{[player, dealer].min.name} won!"
+    elsif player == dealer
+      "It's a tie!"
+    else
+      "#{[dealer, player].max.name} won!"
+    end
+  end
+
+  def dealer_turn
+    dealer.draw_card(deck) until dealer.stay?
+  end
+
+  def player_turn
+    loop do
+      choice = player.prompt
+      break if choice == 's'
+
+      player.draw_card(deck)
+      break if player.busted?
+
+      show_hands
+    end
+  end
+
   def print_welcome_message
     system 'clear'
-    puts '=== Welcome to 21! ==='
+    puts ' Welcome to 21! '.center(MENU_WIDTH, '=')
   end
 
   def show_hands(hide_cards: true)
     print_welcome_message
     dealer.show_hand(hide_cards: hide_cards)
     player.show_hand
-    puts '------'
+    puts '-' * MENU_WIDTH
   end
 
   def deal_starting_hand
@@ -181,8 +229,5 @@ class Game
     end
   end
 end
-
-# deck = Deck.new
-# deck.instance_variable_get(:@cards).each {|card| puts "#{card} : #{card.value}"}
 
 Game.new.play
